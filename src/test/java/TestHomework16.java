b9e1ac59e07ebc89f8c42e1c8198701ecd8775bc
dariaproit <dariad.pro.it@gmail.com>
2023-06-30 00:02:13 -0700

    Finished homework 16 assignment

---
+++ src/test/java/TestHomework16.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import java.time.Duration;
+
+public class TestHomework16 {
+    @Test
+    public void registrationNavigation() {
+
+        //      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement registrationLink = driver.findElement(By.id("hel"));
+        registrationLink.click();
+
+        String actualRegistrationURL = driver.getCurrentUrl();
+        String expectedRegistrationURL = "https://qa.koel.app/registration.php";
+        Assert.assertEquals(actualRegistrationURL,expectedRegistrationURL, "Wrong registration URL");
+
+        driver.quit();
+    }
+}
